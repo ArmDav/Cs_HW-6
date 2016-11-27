1135044ea09a24cf85c37996d8698d0edc9c9563
ArmDav <armdav13@gmail.com>
2016-11-27 19:27:25 +0400

    Update driver.java

--- driver.java
+++ driver.java
@@ -9,7 +9,7 @@ public class Driver {
 
 		Rectangle rct = new Rectangle(50, 80);
 		System.out.println("the area of your rectangle is " + rct.getArea());
-		double number = 8.6;
+		double number = 15.6;
 		System.out.println(Math.factorial((int) number));
 		System.out.println(Math.factorialLooped((int) number));
 	}
